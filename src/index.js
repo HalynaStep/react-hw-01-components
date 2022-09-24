f5625c3a38d22b0952c61856c33dbc7a2d77d666
HalynaStep <gstep2003@gmail.com>
2022-09-24 21:20:15 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
 import './index.css';
 import {theme} from 'theme.jsx'
 
